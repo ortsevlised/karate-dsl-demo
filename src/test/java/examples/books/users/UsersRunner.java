d6be4b0e26342ba99933d9f90180b00c9da79fdf
Jorge <jdesilvestro@gmail.com>
2021-01-06 15:11:13 +0000

    adding some examples

---
+++ src/test/java/examples/books/users/UsersRunner.java
@@ -0,0 +1,12 @@
+package examples.books.users;
+
+import com.intuit.karate.junit5.Karate;
+
+class UsersRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("books").relativeTo(getClass());
+    }    
+
+}
